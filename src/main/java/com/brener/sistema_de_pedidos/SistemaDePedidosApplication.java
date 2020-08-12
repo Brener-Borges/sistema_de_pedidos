1a871c3677e79072c1d2c542b1206302a1e1aa63
Brener Pereira Borges <brenerborges@gmail.com>
2020-08-12 12:07:27 -0300

    Criando operacao de instanciacao

--- src/main/java/com/brener/sistema_de_pedidos/SistemaDePedidosApplication.java
+++ src/main/java/com/brener/sistema_de_pedidos/SistemaDePedidosApplication.java
@@ -1,13 +1,31 @@
 package com.brener.sistema_de_pedidos;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class SistemaDePedidosApplication {
+import com.brener.sistema_de_pedidos.domain.Categoria;
+import com.brener.sistema_de_pedidos.repositories.CategoriaRepository;
 
+@SpringBootApplication
+public class SistemaDePedidosApplication implements CommandLineRunner {
+
+	@Autowired
+	private CategoriaRepository categoriaRepository;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(SistemaDePedidosApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		Categoria cat1 = new Categoria(null, "Informática");
+		Categoria cat2 = new Categoria(null, "Escritório");
+		
+		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));	
 	}
 
 }
