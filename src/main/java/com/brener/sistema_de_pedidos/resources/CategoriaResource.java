9f456693a37b19df8e1c9237a7d70022df0dd136
Brener Pereira Borges <brenerborges@gmail.com>
2020-08-22 13:55:59 -0300

    Atualizando Categoria com PUT

--- src/main/java/com/brener/sistema_de_pedidos/resources/CategoriaResource.java
+++ src/main/java/com/brener/sistema_de_pedidos/resources/CategoriaResource.java
@@ -7,6 +7,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -37,5 +38,10 @@ public class CategoriaResource {
 		
 	}
 	
-	
+	@PutMapping("/{id}")
+	public ResponseEntity<Void> update(@PathVariable Integer id, @RequestBody Categoria obj){
+		obj.setId(id);
+		service.update(obj);
+		return ResponseEntity.noContent().build();
+	}
 }
