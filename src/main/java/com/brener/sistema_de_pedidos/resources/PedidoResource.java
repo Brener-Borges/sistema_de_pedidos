16b8d18323338ba5d7d8793adaf36d7219e2a256
Brener Pereira Borges <brenerborges@gmail.com>
2020-09-15 13:29:53 -0300

    Inserindo Pedido

--- src/main/java/com/brener/sistema_de_pedidos/resources/PedidoResource.java
+++ src/main/java/com/brener/sistema_de_pedidos/resources/PedidoResource.java
@@ -1,11 +1,18 @@
 package com.brener.sistema_de_pedidos.resources;
 
+import java.net.URI;
+
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.brener.sistema_de_pedidos.domain.Pedido;
 import com.brener.sistema_de_pedidos.services.PedidoService;
@@ -21,5 +28,13 @@ public class PedidoResource {
 	public ResponseEntity<Pedido> findById(@PathVariable Integer id){
 		Pedido obj = service.findById(id);
 		return ResponseEntity.ok().body(obj);
+	}
+	
+	@PostMapping
+	public ResponseEntity<Void> insert(@RequestBody Pedido obj){
+		obj = service.insert(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
+				.path("/{id}").buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).build();
 	}
 }
--- src/main/java/com/brener/sistema_de_pedidos/services/PedidoService.java
+++ src/main/java/com/brener/sistema_de_pedidos/services/PedidoService.java
@@ -1,11 +1,18 @@
 package com.brener.sistema_de_pedidos.services;
 
+import java.util.Date;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.brener.sistema_de_pedidos.domain.ItemPedido;
+import com.brener.sistema_de_pedidos.domain.PagamentoComBoleto;
 import com.brener.sistema_de_pedidos.domain.Pedido;
+import com.brener.sistema_de_pedidos.domain.enums.EstadoPagamento;
+import com.brener.sistema_de_pedidos.repositories.ItemPedidoRepository;
+import com.brener.sistema_de_pedidos.repositories.PagamentoRepository;
 import com.brener.sistema_de_pedidos.repositories.PedidoRepository;
 import com.brener.sistema_de_pedidos.services.exceptions.ObjectNotFoundException;
 
@@ -15,8 +22,41 @@ public class PedidoService {
 	@Autowired
 	private PedidoRepository repository;
 	
+	@Autowired
+	private PagamentoRepository pagamentoRepository;
+	
+	@Autowired
+	private ItemPedidoRepository itemPedidoRepository;
+	
+	@Autowired
+	private BoletoService boletoService;
+	
+	@Autowired
+	private ProdutoService produtoService;
+	
 	public Pedido findById(Integer id) {
 		Optional<Pedido> obj = repository.findById(id);
 		return obj.orElseThrow( () -> new ObjectNotFoundException("Objeto não encontrado! Id: " + id + ", Tipo: " + Pedido.class.getName()));
+	}
+	
+	@Transactional
+	public Pedido insert(Pedido obj) {
+		obj.setId(null);
+		obj.setInstante(new Date());
+		obj.getPagamento().setEstado(EstadoPagamento.PENDENTE);
+		obj.getPagamento().setPedido(obj);
+		if (obj.getPagamento() instanceof PagamentoComBoleto) {
+			PagamentoComBoleto pagto = (PagamentoComBoleto)obj.getPagamento();
+			boletoService.preencherPagamentoComBoleto(pagto, obj.getInstante());
+		}
+		obj = repository.save(obj);
+		pagamentoRepository.save(obj.getPagamento());
+		for (ItemPedido ip : obj.getItens()) {
+			ip.setDesconto(0.0);
+			ip.setPreco(produtoService.findById(ip.getProduto().getId()).getPreco());
+			ip.setPedido(obj);
+		}
+		itemPedidoRepository.saveAll(obj.getItens());
+		return obj;
 	}
 }
