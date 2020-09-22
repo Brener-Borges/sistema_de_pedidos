fa93fccf09a4d9ed9f4173e02a418396b2579188
Brener Pereira Borges <brenerborges@gmail.com>
2020-09-22 13:42:12 -0300

    Email HTML

--- src/main/java/com/brener/sistema_de_pedidos/services/EmailService.java
+++ src/main/java/com/brener/sistema_de_pedidos/services/EmailService.java
@@ -1,5 +1,7 @@
 package com.brener.sistema_de_pedidos.services;
 
+import javax.mail.internet.MimeMessage;
+
 import org.springframework.mail.SimpleMailMessage;
 
 import com.brener.sistema_de_pedidos.domain.Pedido;
@@ -10,4 +12,7 @@ public interface EmailService {
 	
 	void sendEmail(SimpleMailMessage msg);
 
+	void sendOrderConfirmationHtmlEmail(Pedido obj);
+	
+	void sendHtmlEmail(MimeMessage msg);
 }
--- src/main/java/com/brener/sistema_de_pedidos/services/MockEmailService.java
+++ src/main/java/com/brener/sistema_de_pedidos/services/MockEmailService.java
@@ -1,5 +1,7 @@
 package com.brener.sistema_de_pedidos.services;
 
+import javax.mail.internet.MimeMessage;
+
 import org.slf4j.Logger;
 import org.springframework.mail.SimpleMailMessage;
 
@@ -10,6 +12,13 @@ public class MockEmailService extends AbstractEmailService {
 	@Override
 	public void sendEmail(SimpleMailMessage msg) {
 		LOG.info("Simulando envio de email...");
+		LOG.info(msg.toString());
+		LOG.info("Email enviado");
+	}
+
+	@Override
+	public void sendHtmlEmail(MimeMessage msg) {
+		LOG.info("Simulando envio de email HTML...");
 		LOG.info(msg.toString());
 		LOG.info("Email enviado");
 	}
